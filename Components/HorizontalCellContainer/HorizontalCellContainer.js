120612bde0f60712184dfa7095154b70adb7a755
Abhishek <abhishek@imoneypay.in>
2023-02-22 13:42:22 +0530

    horizontal cell container

--- Components/HorizontalCellContainer/HorizontalCellContainer.js
+++ Components/HorizontalCellContainer/HorizontalCellContainer.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {Colors} from '../Utils/Colors';
 
 export const HorizontalCellContainer = () => {
-  return <View></View>;
+  return <View style={styles.container}></View>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    backgroundColor: Colors.black,
+  },
+});
